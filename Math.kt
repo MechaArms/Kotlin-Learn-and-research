f82ad1ad63d9be0048b33bf6f37afdabdb9bb556
Romero <103903074+MechaArms@users.noreply.github.com>
2022-08-31 15:13:49 -0300

    Math

--- Math.kt
+++ Math.kt
@@ -3,38 +3,38 @@
 
 // Math.pow() returns the first value raised to the power of the second value. It accepts and returns Double values.
 
-    Math.pow(5.0, 3.0) // 125.0
+Math.pow(5.0, 3.0) // 125.0
 
 // Math.min() returns the minimum value from two given numbers.
 
-    Math.min(5, 3) // 3
+Math.min(5, 3) // 3
 
 // Alternatively, Math.max() determines the maximum value.
 
-	  Math.max(5, 3) // 5
+Math.max(5, 3) // 5
 
 // Math.random() returns a randomly generated number between 0 and 1.
 
-    Math.random() // 0.3335735290527727
+Math.random() // 0.3335735290527727
 
 // Math.round() accepts a single Double value and returns the result of rounding that value to the nearest Integer.
 
-    Math.round(15.7) // 16
+Math.round(15.7) // 16
 
 // Math.ceil() accepts a Double value and returns the smallest value that is greater than or equal to the given value
 
-	  Math.ceil(3.5) // 4.0
+Math.ceil(3.5) // 4.0
  
 // Math.sqrt() accepts a Double value and returns its square root.
  
-  	Math.sqrt(25.0) // 5.0
+Math.sqrt(25.0) // 5.0
 
 // Math.abs() accepts an Int value and returns its abso
 
-  	Math.abs(-20) // 20
+Math.abs(-20) // 20
 
 // Math.floor() accepts a Double value and returns the largest value that is less than or equal to it.
 
-	Math.floor(6.9) // 6.0
+Math.floor(6.9) // 6.0
 
 */
